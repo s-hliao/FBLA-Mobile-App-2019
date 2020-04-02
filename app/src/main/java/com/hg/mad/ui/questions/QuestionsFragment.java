c43992b56ca21ca742e98820d88e69851961521a
Henry <henry.z.liao@hotmail.com>
2020-04-01 22:44:43 -0700

    Questions Fragment File
    Deleted viewmodel inside folder

---
+++ app/src/main/java/com/hg/mad/ui/questions/QuestionsFragment.java
@@ -0,0 +1,20 @@
+package com.hg.mad.ui.questions;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.hg.mad.R;
+
+public class QuestionsFragment extends Fragment {
+
+
+}
