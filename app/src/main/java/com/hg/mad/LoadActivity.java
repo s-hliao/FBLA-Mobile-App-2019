8b0e0051f89729ed538b0b31aea97cba28d61060
georgezhang02 <georgezhang02@hotmail.com>
2020-04-07 13:08:37 -0700

    Finished splash screen and new icon

--- app/src/main/java/com/hg/mad/LoadActivity.java
+++ app/src/main/java/com/hg/mad/LoadActivity.java
@@ -3,30 +3,14 @@ package com.hg.mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
 
 public class LoadActivity extends AppCompatActivity {
-
-    private FirebaseUser user;
-    private AppCompatActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load);
 
-        activity = this;
-        user = FirebaseAuth.getInstance().getCurrentUser();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                StaticMethods.redirect(activity);
-            }
-        }, 500);
+        StaticMethods.redirect(this);
+        finish();
     }
 }
