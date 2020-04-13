1df1fa5ec8754c67bb66b8dd0794c1189197fe96
georgezhang02 <georgezhang02@hotmail.com>
2020-04-13 13:15:27 -0700

    Updated byevents

--- app/src/main/java/com/hg/mad/ui/ByEventsFragment.java
+++ app/src/main/java/com/hg/mad/ui/ByEventsFragment.java
@@ -1,90 +1,35 @@
 package com.hg.mad.ui;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.hg.mad.R;
-import com.hg.mad.adapter.MyCompEventAdapter;
-import com.hg.mad.dialog.CompSUDialogFragment;
+import com.hg.mad.dialog.CompUsersSUDialogFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+public class ByEventsFragment extends CompetitiveEventsFragment{
 
-public class ByEventsFragment extends Fragment
-        implements MyCompEventAdapter.OnMyCompListener {
+    @Override
+    protected void setVisibility() {
+        manageButton.setVisibility(View.VISIBLE);
+        manageButton.setText("Reset All Events");
+    };
 
-    private View root;
+    @Override
+    void onManageClicked() {
+        // RESET ALL EVENTS
 
-    private RecyclerView myCompRecycler;
-    private LinearLayoutManager layoutManager;
-    private MyCompEventAdapter adapter;
-    private List<String> myComp;
-
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        this.root = inflater.inflate(R.layout.fragment_mycomp, container, false);
-
-        // Recycler Views
-        myComp = new ArrayList<>();
-        myCompRecycler = root.findViewById(R.id.recycler_my_comp);
-        layoutManager = new LinearLayoutManager(getContext());
-        myCompRecycler.setLayoutManager(layoutManager);
-        myCompRecycler.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
-
-        updateCompAdapter();
-        beganUpdating();
-
-        return root;
-    }
-
-    private void beganUpdating() {
-        DocumentReference databaseUserRef = FirebaseFirestore.getInstance().collection("DatabaseUser")
-                .document(FirebaseAuth.getInstance().getCurrentUser().getUid());
-
-        // Set up competitive events listener
-        databaseUserRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
-
-                if (snapshot != null && snapshot.exists() && snapshot.get("competitiveEvents") != null) {
-                    myComp = new ArrayList<>();
-                    Map<String, Integer> events = (Map<String, Integer>) snapshot.get("competitiveEvents");
-                    myComp.addAll(events.keySet());
-
-                    updateCompAdapter();
-                }
-            }
-        });
-    }
-
-    private void updateCompAdapter(){
-        adapter = new MyCompEventAdapter(myComp, this);
-        myCompRecycler.setAdapter(adapter);
     }
 
     @Override
-    public void onMyCompSelected(String eventName) {
+    public void onCompetitiveSelected(DocumentSnapshot competitiveEvent) {
 
-        CompSUDialogFragment compSUDialog = new CompSUDialogFragment();
-        compSUDialog.setEventName(eventName);
+        hideKeyboard();
+        searchView.clearFocus();
 
-        getFragmentManager().executePendingTransactions();
-        if (!compSUDialog.isAdded())
-            compSUDialog.show(getFragmentManager(), "CompSUDialog");
+        final String eventName = competitiveEvent.get("eventName").toString();
+
+        // Show the users who signed up for this event
+        CompUsersSUDialogFragment compUsersSUDialog = new CompUsersSUDialogFragment();
+        compUsersSUDialog.setEventName(eventName);
+        compUsersSUDialog.show(getFragmentManager(), "CompUsersSUDialog");
     }
 }
