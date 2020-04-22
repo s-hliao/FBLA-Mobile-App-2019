433881b5a855af3f6f98e81e4c4a2fac2fcb65e5
Henry <henry.z.liao@hotmail.com>
2020-04-21 22:17:06 -0700

    RI3d more like Calendar in 3 Hours
    With Live Updating

--- app/src/main/java/com/hg/mad/ui/ChapterCalendarFragment.java
+++ app/src/main/java/com/hg/mad/ui/ChapterCalendarFragment.java
@@ -1,7 +1,10 @@
 package com.hg.mad.ui;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,152 +14,176 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.hg.mad.ManageCompActivity;
 import com.hg.mad.R;
+import com.hg.mad.adapter.ChapterEventAdapter;
 import com.hg.mad.adapter.CompetitiveAdapter;
+import com.hg.mad.dialog.ChapEventDialogFragment;
 import com.hg.mad.dialog.CompSUDialogFragment;
 import com.hg.mad.dialog.CompetitiveDialogFragment;
 import com.hg.mad.dialog.FilterDialogFragment;
 import com.hg.mad.dialog.Filters;
+import com.hg.mad.dialog.TakeAttendanceDialogFragment;
 import com.hg.mad.model.DatabaseUser;
+import com.hg.mad.util.ThisUser;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 
-public class ChapterCalendarFragment extends Fragment implements
-        View.OnClickListener,
-        FilterDialogFragment.FilterListener,
-        CompetitiveAdapter.OnCompetitiveListener {
+public class ChapterCalendarFragment extends Fragment implements ChapterEventAdapter.OnChapListener {
 
-    private RecyclerView competitiveRecycler;
-    SearchView searchView;
-    TextView manageButton;
-
-    private FirebaseFirestore firestore;
+    private CalendarView eventCalendar;
+    private RecyclerView eventRecycler;
+    private CollectionReference chapterEvents;
+    private List<EventDay> events;
+    private ChapterEventAdapter adapter;
     private Query query;
 
-    private FilterDialogFragment filterDialog;
-    private CompetitiveAdapter adapter;
-    private Filters filters;
-    private String searchText;
+    private TakeAttendanceDialogFragment attendanceDialog;
+    private ChapEventDialogFragment chapEventDialog;
 
-    private Map<String, Integer> eventsSignedUp;
-
-    private CompetitiveDialogFragment competitiveDialog;
-    private CompSUDialogFragment compSUDialog;
-
+    @SuppressLint("ResourceType")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_chapter_calendar, container, false);
 
-        // Set up views
-        competitiveRecycler = root.findViewById(R.id.recycler_competitive);
-        searchView = root.findViewById(R.id.search_competitive);
-        ImageView filterButton = root.findViewById(R.id.button_filter);
-        manageButton = root.findViewById(R.id.manage_signups);
-        filterButton.setOnClickListener(this);
-        manageButton.setOnClickListener(this);
+        eventCalendar = root.findViewById(R.id.calendarView);
+        eventRecycler = root.findViewById(R.id.event_recycler);
+        events = new ArrayList<>();
 
-        // Enable Firestore logging
-        FirebaseFirestore.setLoggingEnabled(true);
+        attendanceDialog = new TakeAttendanceDialogFragment();
+        chapEventDialog = new ChapEventDialogFragment();
 
-        // Initialize Firestore and the main RecyclerView
-        initFirestore();
+        eventCalendar.setHeaderColor(Color.parseColor("#3666a8"));
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.YEAR, 4);
+        eventCalendar.setMaximumDate(c);
+        c = Calendar.getInstance();
 
-        // Only show manage to admins
-        setVisibility();
+        c.add(Calendar.YEAR,-4);
+        eventCalendar.setMinimumDate(c);
 
-        initRecyclerView();
+        final ChapterEventAdapter.OnChapListener o = this;
 
-        // Filter Dialog
-        filterDialog = new FilterDialogFragment();
-        filters = Filters.getDefault();
+        FirebaseFirestore.getInstance().collection("Chapter")
+                .whereEqualTo("chapterName", ThisUser.getChapterName()).get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-        initSearch();
+                        chapterEvents = queryDocumentSnapshots.getDocuments().get(0).getReference()
+                                            .collection("ChapterEvent");
 
-        // Competitive Dialogs
-        competitiveDialog = new CompetitiveDialogFragment();
-        compSUDialog = new CompSUDialogFragment();
+                        chapterEvents.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for(DocumentSnapshot ds:queryDocumentSnapshots){
+                                    System.out.println("done");
+                                    Date d  = ds.getDate("date");
+                                    Calendar c = Calendar.getInstance();
+                                    c.setTime(d);
+                                    events.add(new EventDay(c, R.drawable.dot, Color.parseColor("#314fbd")));
+                                }
+                                eventCalendar.setEvents(events);
+                            }
+                        });
 
-        // Manage sign ups
-        manageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onManageClicked();
-            }
-        });
+
+
+                        chapterEvents.addSnapshotListener((snapshots, e) -> {
+                            if (e != null) {
+                                System.out.println("listen:error"+e);
+                                return;
+                            }
+                            events = new ArrayList<>();
+                            for(DocumentSnapshot ds:snapshots){
+                                System.out.println("done");
+                                Date d  = ds.getDate("date");
+                                Calendar c1 = Calendar.getInstance();
+                                c1.setTime(d);
+                                events.add(new EventDay(c1, R.drawable.dot, Color.parseColor("#314fbd")));
+                            }
+                            eventCalendar.setEvents(events);
+
+                            System.out.println("events changed");
+
+
+                        });
+
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                        String s = dateFormat.format(Calendar.getInstance().getTime());
+
+                        try {
+                            query = chapterEvents.whereEqualTo("date", dateFormat.parse(s)).orderBy("lower");
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+
+                        adapter = new ChapterEventAdapter(query, o);
+                        adapter.setQuery(query);
+
+                        eventRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+                        eventRecycler.setAdapter(adapter);
+                        eventRecycler.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+                        eventCalendar.setOnDayClickListener(new OnDayClickListener() {
+
+                            @Override
+                            public void onDayClick(EventDay eventDay) {
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+                                String s = dateFormat.format(eventDay.getCalendar().getTime());
+
+                                System.out.println(s);
+
+                                try {
+                                    query = chapterEvents.whereEqualTo("date", dateFormat.parse(s)).orderBy("eventName");
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                                adapter.setQuery(query);
+
+                            }
+                        });
+
+                    }
+                });
 
         return root;
     }
 
-    protected void setVisibility() {
-        DocumentReference databaseUserRef = firestore.collection("DatabaseUser").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
-        databaseUserRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DatabaseUser databaseUserClass = task.getResult().toObject(DatabaseUser.class);
 
-                    if (databaseUserClass.getIsAdmin()){
-                        manageButton.setVisibility(View.VISIBLE);
-                    }
-                }
-            }
-        });
-    }
-
-    private void initFirestore() {
-        firestore = FirebaseFirestore.getInstance();
-
-        query = firestore.collection("CompetitiveEvents")
-                .orderBy("eventName", Query.Direction.ASCENDING);
-    }
-
-    private void initRecyclerView() {
-
-        adapter = new CompetitiveAdapter(query, this);
-
-        competitiveRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
-        competitiveRecycler.setAdapter(adapter);
-        competitiveRecycler.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
-    }
-
-    private void initSearch() {
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                searchView.clearFocus();
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                searchText = newText;
-                onFilter(filters);
-
-                return false;
-            }
-        });
-    }
 
     @Override
     public void onStart() {
         super.onStart();
-
-        // Apply filters
-        onFilter(filters);
-
         // Start listening for Firestore updates
         if (adapter != null) {
             adapter.startListening();
@@ -171,119 +198,60 @@ public class ChapterCalendarFragment extends Fragment implements
             adapter.stopListening();
         }
 
-        hideKeyboard();
-    }
-
-    void hideKeyboard() {
-        searchView.clearFocus();
-        View view = getActivity().getCurrentFocus();
-        if (view != null) {
-            ((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE))
-                    .hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
     }
 
     @Override
-    public void onFilter(Filters filters) {
-        // Construct query basic query
-        Query query = firestore.collection("CompetitiveEvent");
-
-        // Name (contains filter)
-        if (searchText != null){
-
-            String searchLower = searchText.toLowerCase();
-
-            query = firestore.collection("CompetitiveEvent")
-                    .orderBy("lower")
-                    .startAt(searchLower)
-                    .endAt(searchLower+"\uf8ff");
-        }
-
-        // Type (equality filter)
-        if (filters.hasType()) {
-            query = query.whereEqualTo("type", filters.getType());
-        }
-
-        // Category (equality filter)
-        if (filters.hasCategory()) {
-            query = query.whereEqualTo("category", filters.getCategory());
-        }
-
-        // Intro (equality filter)
-        if (filters.hasIntro()) {
-            query = query.whereEqualTo("intro", filters.getIntro());
-        }
-
-        // Update the query
-        this.query = query;
-        adapter.setQuery(query);
-
-        // Save filters
-        this.filters = filters;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_filter:
-                onFilterClicked();
-                break;
-            case R.id.manage_signups:
-                onManageClicked();
-                break;
-        }
-    }
-
-    private void onFilterClicked() {
-        // Show the dialog containing filter options
-
-        hideKeyboard();
-        searchView.clearFocus();
-
-        if (!filterDialog.isAdded())
-            filterDialog.show(getFragmentManager(), "FilterDialog");
-    }
-
-    void onManageClicked() {
-        startActivity(ManageCompActivity.createIntent(getContext()));
-    }
-
-    @Override
-    public void onCompetitiveSelected(DocumentSnapshot competitiveEvent) {
-
-        hideKeyboard();
-        searchView.clearFocus();
-
-        final String eventName = competitiveEvent.get("eventName").toString();
-
-        // Find out if the user has signed up for this event already
-        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DocumentReference userRef = FirebaseFirestore.getInstance().collection("DatabaseUser").document(uid);
-
-        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+    public void onChapSelected(DocumentSnapshot chapEvent) {
+        FirebaseFirestore.getInstance().collection("Chapter")
+                .whereEqualTo("chapterName", ThisUser.getChapterName()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful() && task.getResult() != null) {
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                DocumentSnapshot chapRef = queryDocumentSnapshots.getDocuments().get(0);
+                Map<String,  Map<String, Map<String, Object>>> events = (Map) chapRef.get("chapterEvents");
+                String eventName = (String) chapEvent.get("eventName");
 
-                    eventsSignedUp = (Map<String, Integer>) task.getResult().get("competitiveEvents");
+                if (events.containsKey(eventName) && events.get(eventName).containsKey(ThisUser.getUid())){
+                    boolean attendance = chapEvent.getBoolean("attendanceActive");
+                    attendanceDialog.setAttendanceActive(attendance);
+                    attendanceDialog.setEventName(chapEvent.get("eventName").toString());
 
-                    // Show the already signed up dialog
-                    if (eventsSignedUp.containsKey(eventName)) {
-                        compSUDialog.setEventName(eventName);
+                    if(attendance)attendanceDialog.setAttendancePassword(chapEvent.get("signInKey").toString());
 
-                        getFragmentManager().executePendingTransactions();
-                        if (!compSUDialog.isAdded())
-                            compSUDialog.show(getFragmentManager(), "CompSUDialog");
+                    showAttendance(chapEvent);
+
+                } else {
+                    chapEventDialog.setEventName(eventName);
+
+                    getFragmentManager().executePendingTransactions();
+                    if (!chapEventDialog.isAdded())
+                        chapEventDialog.show(getFragmentManager(), "ChapEventsDialog");
+                }
+            }
+        });
+    }
+
+    private void showAttendance(final DocumentSnapshot s){
+        FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
+        final CollectionReference chaptersCollection = fireStore.collection("Chapter");
+
+        // Update Chapter
+        chaptersCollection.whereEqualTo("chapterName", ThisUser.getChapterName())
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    DocumentSnapshot chapter = task.getResult().getDocuments().get(0);
+
+                    Map<String, Map<String, Map<String, Object>>> currentEventsChap = (Map) chapter.get("chapterEvents");
+                    Map<String, Object> user = currentEventsChap.get(s.get("eventName")).get(ThisUser.getUid());
+                    if ((boolean) user.get("signedIn")) {
+                        attendanceDialog.setAttendanceActive(false);
                     }
 
-                    // Show the signed up dialog
-                    else {
-                        competitiveDialog.setEventName(eventName);
-
-                        getFragmentManager().executePendingTransactions();
-                        if (!competitiveDialog.isAdded())
-                            competitiveDialog.show(getFragmentManager(), "CompetitiveDialog");
-                    }
+                    getFragmentManager().executePendingTransactions();
+                    if(!attendanceDialog.isAdded())
+                        attendanceDialog.show(getFragmentManager(), "takeAttendanceDialog");
                 }
             }
         });
